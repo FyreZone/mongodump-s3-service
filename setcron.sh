1d09944565af7f6aabb8fc9fa1a31fce361fe1ee
Román Benjámin <svetch.game@gmail.com>
2023-09-15 11:21:40 +0200

    feat: add debug message at start

--- setcron.sh
+++ setcron.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 echo "$CRON_EXPRESSION bash /usr/local/bin/myawesomescript.sh" > /etc/crontabs/root
+echo "writed cron -> $CRON_EXPRESSION bash /usr/local/bin/myawesomescript.sh"
 crond -S -l 0 -f
